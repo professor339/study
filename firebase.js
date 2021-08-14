70410d77ba3370bbfd762a626c2229e5aea60a23
professor339 <65883904+professor339@users.noreply.github.com>
2021-08-15 01:26:09 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAH6Tx15yDQ3s7H-XsKqdPk6K70yX9x1xg",
+  authDomain: "third-a7810.firebaseapp.com",
+  projectId: "third-a7810",
+  storageBucket: "third-a7810.appspot.com",
+  messagingSenderId: "348439101337",
+  appId: "1:348439101337:web:607510de6e57b348025d08"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+
